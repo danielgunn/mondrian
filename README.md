a43f279f00fc387c12bc8962a1fee743100f98a8
Daniel Gunn <dangunn@gmail.com>
2020-06-30 16:53:28 +0800

    - Minimum rectangle dimensions now controlled by mouse coordinates - Clicking mouse now generates new painting

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Mondrian generator
 [Piet Mondrian](https://en.wikipedia.org/wiki/Piet_Mondrian) style art generator
 
+## Features
+- Click mouse to generate new painting
+- Mouse coordinate (relative to top left corner) controls the minimum rectangle size
+
 ## For example:
 
 ![](demo.gif)
--- mondrian_recursive.py
+++ mondrian_recursive.py
@@ -5,37 +5,52 @@ from random import randint
 colors = [(0xff,0xff,0xff),(0xfa, 0xc9, 0x01),(0x22, 0x50, 0x95),(0xdd,0x01,0x00)]
 width = 612
 height = 1024
+min_scaling_factor = 0.8
 
-min_rectangle_width = int(0.3 * width)
-min_rectangle_height = int(0.3 * height)
+def draw_mondrian(
+        screen,
+        min_rectangle_width = int(0.3 * width),
+        min_rectangle_height = int(0.3 * height),
+        top_left=Vector2(0,0),
+        bottom_right=Vector2(width,height)):
 
-def draw_mondrian(screen, top_left=Vector2(0,0), bottom_right=Vector2(width,height)):
+    # draw this rectangle
     c = colors[randint(0, len(colors)-1)]
     screen.fill(c, pygame.Rect(top_left, bottom_right))
     pygame.draw.rect(screen, (0,0,0), pygame.Rect(top_left, bottom_right), 10)
-    can_split_y = (top_left[1] + min_rectangle_height) < 0.8 * bottom_right[1]
-    can_split_x = (top_left[0] + min_rectangle_width) < 0.8 * bottom_right[0]
+
+    max_rectangle_width = int(min_scaling_factor * (bottom_right[0] - top_left[0]))
+    max_rectangle_height = int(min_scaling_factor * (bottom_right[1] - top_left[1]))
+
+    can_split_x = 2*min_rectangle_width < max_rectangle_width
+    can_split_y = 2*min_rectangle_height < max_rectangle_height
+
+    if not(can_split_x or can_split_y):
+        return
+
+    if can_split_x:
+        x_split = top_left[0] + randint(min_rectangle_width, max_rectangle_width - min_rectangle_width)
+
+    if can_split_y:
+        y_split = top_left[1] + randint(min_rectangle_height, max_rectangle_height - min_rectangle_height)
+
     if can_split_x and can_split_y:
         if randint(0,1) == 0:
-            y_split = randint((top_left[1] + min_rectangle_height), int(0.8 * bottom_right[1]))
-            draw_mondrian(screen, top_left, Vector2(bottom_right[0], y_split))
-            draw_mondrian(screen, Vector2(top_left[0], y_split), bottom_right)
+            draw_mondrian(screen, min_rectangle_width, min_rectangle_height, top_left, Vector2(bottom_right[0], y_split))
+            draw_mondrian(screen, min_rectangle_width, min_rectangle_height, Vector2(top_left[0], y_split), bottom_right)
         else :
-            x_split= randint((top_left[0] + min_rectangle_width), int(0.8 * bottom_right[0]))
-            draw_mondrian(screen, top_left, Vector2(x_split, bottom_right[1]))
-            draw_mondrian(screen, Vector2(x_split, top_left[1]), bottom_right)
+            draw_mondrian(screen, min_rectangle_width, min_rectangle_height, top_left, Vector2(x_split, bottom_right[1]))
+            draw_mondrian(screen, min_rectangle_width, min_rectangle_height, Vector2(x_split, top_left[1]), bottom_right)
     elif can_split_x:
-        x_split = randint((top_left[0] + min_rectangle_width), int(0.8 * bottom_right[0]))
-        draw_mondrian(screen, top_left, Vector2(x_split, bottom_right[1]))
-        draw_mondrian(screen, Vector2(x_split, top_left[1]), bottom_right)
+        draw_mondrian(screen, min_rectangle_width, min_rectangle_height, top_left, Vector2(x_split, bottom_right[1]))
+        draw_mondrian(screen, min_rectangle_width, min_rectangle_height, Vector2(x_split, top_left[1]), bottom_right)
     elif can_split_y:
-        y_split = randint((top_left[1] + min_rectangle_height), int(0.8 * bottom_right[1]))
-        draw_mondrian(screen, top_left, Vector2(bottom_right[0], y_split))
-        draw_mondrian(screen, Vector2(top_left[0], y_split), bottom_right)
+        draw_mondrian(screen, min_rectangle_width, min_rectangle_height, top_left, Vector2(bottom_right[0], y_split))
+        draw_mondrian(screen, min_rectangle_width, min_rectangle_height, Vector2(top_left[0], y_split), bottom_right)
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Mondrian -- press space for another")
+pygame.display.set_caption("Mondrian")
 
 draw_mondrian(screen)
 
@@ -45,8 +60,8 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                draw_mondrian(screen)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            mousePos = pygame.mouse.get_pos()
+            draw_mondrian(screen, mousePos[0], mousePos[1])
 
     pygame.display.flip()
